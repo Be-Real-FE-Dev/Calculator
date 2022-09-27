26cb70a8223c7846857a39be5e09e3c47a277601
harseille <joonhabaak@gmail.com>
2022-09-27 15:11:21 +0900

    :sparkles: add readme

--- HwangPark/react-calculate/src/components/Calculator/DigitButtons.jsx
+++ HwangPark/react-calculate/src/components/Calculator/DigitButtons.jsx
@@ -1,54 +1,41 @@
 import styled from 'styled-components';
 import Button from '../UI/Button';
 
+const DigitButton = styled(Button)`
+  grid-area: ${(props) => props.digit};
+  width: 100%;
+`;
+
 const DigitButtonWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-areas: 'seven eghit nine' 'four five six' 'one two three' 'zero zero zero';
-  button {
-    width: 100%;
-  }
-
-  button: first-child {
-    grid-area: zero;
-  }
-  button: nth-child(2) {
-    grid-area: one;
-  }
-  button: nth-child(3) {
-    grid-area: two;
-  }
-  button: nth-child(4) {
-    grid-area: three;
-  }
-  button: nth-child(5) {
-    grid-area: four;
-  }
-  button: nth-child(6) {
-    grid-area: five;
-  }
-  button: nth-child(7) {
-    grid-area: six;
-  }
-  button: nth-child(8) {
-    grid-area: seven;
-  }
-  button: nth-child(9) {
-    grid-area: eghit;
-  }
-  button: last-child {
-    grid-area: nine;
-  }
+  grid-template-areas: 'seven eight nine' 'four five six' 'one two three' 'zero zero zero';
 `;
-const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const digits = [
+  { digit: 0, str_num: 'zero' },
+  { digit: 1, str_num: 'one' },
+  { digit: 2, str_num: 'two' },
+  { digit: 3, str_num: 'three' },
+  { digit: 4, str_num: 'four' },
+  { digit: 5, str_num: 'five' },
+  { digit: 6, str_num: 'six' },
+  { digit: 7, str_num: 'seven' },
+  { digit: 8, str_num: 'eight' },
+  { digit: 9, str_num: 'nine' },
+];
 
 const DigitButtons = (props) => {
   return (
     <DigitButtonWrapper>
-      {digits.map((digit) => (
-        <Button key={digit} id={digit} onClick={props.onClickNumber}>
+      {digits.map(({ digit, str_num }) => (
+        <DigitButton
+          key={digit}
+          id={digit}
+          digit={str_num}
+          onClick={props.onClickNumber}
+        >
           {digit}
-        </Button>
+        </DigitButton>
       ))}
     </DigitButtonWrapper>
   );
